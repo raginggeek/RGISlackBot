df486a4b6cdfc37a456a32ee40d1e40ab6c973c5
Geoff Finch <gfinch1@gmail.com>
2019-03-07 19:32:51 -0600

    initial skeleton of Slackbot.

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='RGISlackBot',
+    version='0.0.1',
+    packages=['venv.Lib.site-packages.idna', 'venv.Lib.site-packages.certifi', 'venv.Lib.site-packages.chardet',
+              'venv.Lib.site-packages.chardet.cli', 'venv.Lib.site-packages.urllib3',
+              'venv.Lib.site-packages.urllib3.util', 'venv.Lib.site-packages.urllib3.contrib',
+              'venv.Lib.site-packages.urllib3.contrib._securetransport', 'venv.Lib.site-packages.urllib3.packages',
+              'venv.Lib.site-packages.urllib3.packages.backports',
+              'venv.Lib.site-packages.urllib3.packages.ssl_match_hostname', 'venv.Lib.site-packages.requests',
+              'venv.Lib.site-packages.websocket', 'venv.Lib.site-packages.websocket.tests',
+              'venv.Lib.site-packages.slackclient', 'rgislackbot', 'rgislackbot.tests'],
+    url='https://github.com/raginggeek/RGISlackBot',
+    license='MIT',
+    author='Geoff Finch',
+    author_email='gfinch1@gmail.com',
+    description='A Slackbot Collaboratively Developed for the RGI Slack environment'
+)
